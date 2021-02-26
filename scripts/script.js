8a319d3b8c7ea9b4bebbd112d3ee4285ba424c77
lucasmetron <lucasmetron@gmail.com>
2021-02-26 17:50:28 -0300

    correções finais feitas

--- scripts/script.js
+++ scripts/script.js
@@ -21,11 +21,8 @@ function capturarInf(){
     let resulCarne = (carne * info[0].value) + (carne * info[1].value / 2);
     let resulCerveja = cerveja * info[0].value;
     let resulRefri = (refri * info[0].value) + (refri * info[1].value/2);
-
-
     let horas = info[2].value;
-    let tempo = false
-   
+    
 
     if(info[0].value == "" && info[1].value == "" && info[2].value == ""){
         alert ("Digite os dados necessários para o cálculo.")
@@ -35,10 +32,10 @@ function capturarInf(){
         let resulCarne = (650 * info[0].value) + (650 * info[1].value / 2);
         let resulCerveja = 2000 * info[0].value;
         let resulRefri = (1500 * info[0].value) + (1500 * info[1].value/2);
-        resultado = "<h4>Para seu churrasco será preciso: </h4>" +"- "+resulCarne+" g de carne. "+"<br>"+ "-"+resulCerveja+" ml de Cerveja." +"<br>"+ "-"+resulRefri+" ml de Refrigerante" + "<br>"+ " <img src='./images/drooling-face-emoji.png' alt='fome' style='width: 100px;'>"
+        resultado = "<h4>Para seu churrasco será preciso: </h4>" +"- "+resulCarne/1000+" kg de carne. "+"<br>"+ "- "+Math.ceil(resulCerveja/355)+" latas de Cerveja." +"<br>"+ "- "+Math.ceil(resulRefri/1000)+" litros de bebidas" + "<br>"+ " <img src='./images/drooling-face-emoji.png' alt='fome' style='width: 100px;'>"
 
     } else {
-        resultado = "<h4>Para seu churrasco será preciso: </h4>" +"- "+resulCarne+" g de carne. "+"<br>"+ "-"+resulCerveja+" ml de Cerveja." +"<br>"+ "-"+resulRefri+" ml de Refrigerante" + "<br>"+ " <img src='./images/drooling-face-emoji.png' alt='fome' style='width: 100px;'>"
+        resultado = "<h4>Para seu churrasco será preciso: </h4>" +"- "+resulCarne/1000+" kg de carne. "+"<br>"+ "- "+Math.ceil(resulCerveja/355)+" latas de Cerveja." +"<br>"+ "- "+Math.ceil(resulRefri/1000)+" litros de bebidas" + "<br>"+ " <img src='./images/drooling-face-emoji.png' alt='fome' style='width: 100px;'>"
     }
    
 
